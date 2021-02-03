5a810f0174609562764966e2148bfd597099883f
nguyenhung2710 <72492981+nguyenhung2710@users.noreply.github.com>
2021-02-03 14:43:22 +0700

    ver 5

--- assets/js/dist/main.dev.js
+++ assets/js/dist/main.dev.js
@@ -3,15 +3,18 @@
 jQuery(document).ready(function ($) {
   $(window).scroll(function () {
     if ($(document).scrollTop() > 600) {
-      $(".sec-2__img").addClass("sec-2__animation");
+      //$(".sec-2__img").addClass("sec-2__animation");
+      $(".sec-2__animation").addClass("sec-2__animation-default");
     }
 
     if ($(document).scrollTop() > 1200) {
-      $(".sec-3__img").addClass("sec-3__animation");
+      //$(".sec-3__img").addClass("sec-3__animation");
+      $(".sec-3__animation").addClass("sec-3__animation-default");
     }
 
-    if ($(document).scrollTop() > 2300) {
-      $(".h1st").addClass("h1st__animation");
+    if ($(document).scrollTop() > 2550) {
+      //$(".h1st").addClass("h1st__animation");
+      $(".h1st__animation").addClass("h1st__animation-default");
     }
   });
 });

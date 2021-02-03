4f7ad445e44d4de2437ff6e11b79143a6629a9b6
nguyenhung2710 <72492981+nguyenhung2710@users.noreply.github.com>
2021-02-03 13:48:34 +0700

    ver 4

---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+jQuery(document).ready(function ($) {
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 600) {
+      $(".sec-2__img").addClass("sec-2__animation");
+    }
+
+    if ($(document).scrollTop() > 1200) {
+      $(".sec-3__img").addClass("sec-3__animation");
+    }
+
+    if ($(document).scrollTop() > 2300) {
+      $(".h1st").addClass("h1st__animation");
+    }
+  });
+});
